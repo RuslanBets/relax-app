0f2d938dd0551176cd5d14568f10e1811e60d2ca
RuslanBets <rbec4263@gmail.com>
2022-04-24 14:44:07 +0300

    ruslan-commit

--- app.js
+++ app.js
@@ -5,24 +5,38 @@ const video = document.querySelector('.video__item');
 const audio = document.querySelector('.audio');
 const moodGreen = document.querySelector('._green');
 const moodBlue = document.querySelector('._blue');
+
+const circle = document.querySelector('.circle')
+let lineLength = circle.getTotalLength()
+circle.style.strokeDasharray = lineLength
+circle.style.strokeDashoffset = lineLength
+let progress;
+let lineSong;
+let timeSeconds = 600
+
 let flag = true;
-let miliSeconds = 600000;
-let secondsFuture = Date.now() + 600000;
-let interval;
+let timeLeft = 600000
+let intervalId;
+let intervalId2;
+let lastUpdateTime;
+
 
 let data = {
 	time: time.innerHTML,
 	btnPlayOrStop: "./icon.svg/play.svg",
-
+	btn: '10:00'
 };
 
 buttons.addEventListener('click', event => {
 	if (event.target.localName == 'button') {
 		let num = event.target.innerHTML.split(' ')[0];
 		time.innerHTML = num + ':' + '00';
-		data.time = time.innerHTML; 
-		miliSeconds = Number(event.target.innerHTML.split(' ')[0]) * 60000;
-		secondsFuture = Date.now() + miliSeconds;
+		data.time = time.innerHTML;
+		data.btn = `${event.target.innerHTML.split(' ')[0]}:00`
+		timeLeft = Number(event.target.innerHTML.split(' ')[0]) * 60000
+		timeSeconds = timeLeft / 1000
+		circle.style.strokeDashoffset = lineLength
+		audio.currentTime = 0
 	}
 });
 
@@ -33,37 +47,18 @@ buttonPlay.addEventListener('click', () => {
 		data.btnPlayOrStop = "./icon.svg/pause.svg";
 		video.play();
 		audio.play();
-		interval = setInterval(timer, 100);
+		lastUpdateTime = Date.now()
+		intervalId = setInterval(timer, 100)
+
 	} else if (!flag) {
 		buttonPlay.src = "./icon.svg/play.svg";
 		flag = true;
 		data.btnPlayOrStop = "./icon.svg/play.svg";
 		video.pause();
 		audio.pause();
-		clearInterval(interval);
-		time.innerHTML = String(timer()[0]) + ':' + String(timer()[1]);
+		clearInterval(intervalId)
 	}
 });
-
-
-function timer() {
-	let timeNow = Date.now();
-	let gap = secondsFuture - timeNow;
-	let min = Math.floor(gap / 1000 / 60) % 60;
-	let sec = Math.floor(gap / 1000) % 60;
-	if (gap < 0) {
-		clearInterval(interval); 
-		time.innerHTML = '00:00';
-	}
-	if (min < 10) {
-		min = '0' + min;
-	}
-	if (sec < 10) {
-		sec = '0' + sec;
-	}
-	time.innerHTML = min + ':' + sec;
-	return [min, sec];
-}
 
 moodGreen.onclick = () => {
 	video.children[0].src = "./video/video-forest.webm";
@@ -77,5 +72,34 @@ moodBlue.onclick = () => {
 	audio.src = "./music/maunt.mp3"
 	video.load()
 }
+
+function timer() {
+	const now = Date.now()
+	const timePassed = now - lastUpdateTime;
+	if (timeLeft > timePassed) {
+		timeLeft -= timePassed
+	} else {
+		timeLeft = 0
+		clearInterval(intervalId)
+		audio.pause()
+		video.pause()
+		buttonPlay.src = "./icon.svg/play.svg"
+		audio.currentTime = 0
+	}
+	lastUpdateTime = now
+	let min = Math.floor(timeLeft / 1000 / 60) % 60
+	let sec = Math.floor(timeLeft / 1000) % 60
+	if (min < 10) {
+		min = '0' + min
+	}
+	if (sec < 10) {
+		sec = '0' + sec
+	}
+	time.innerHTML = `${min}:${sec}`
+	lineSong = audio.currentTime
+	progress = lineLength - (lineSong / timeSeconds) * lineLength
+	circle.style.strokeDashoffset = progress
+}
+
 
 
